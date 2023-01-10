2cb9955f1d9f0f5889870169ecd4f32c66e8d67a
Ethan <ethan.tomlinson5912@gmail.com>
2023-01-10 11:49:11 -0500

    Tried to fix understanding rubric thing

--- lib/UnderStantRubrc.dart
+++ lib/UnderStantRubrc.dart
@@ -18,9 +18,6 @@ class UnderStantRubrc extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: [
-            NextBar(
-                  const BATNATest()
-            ),
             SingleChildScrollView(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -196,9 +193,6 @@ class UnderStantRubrc extends StatelessWidget {
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(10, 2, 10, 0),
                       child: TextField(
-                        onChanged: (newVal) {
-                          currentNegotiation.target = newVal as int?;
-                        },
                         controller: TextEditingController(),
                         obscureText: false,
                         textAlign: TextAlign.start,
@@ -309,9 +303,6 @@ class UnderStantRubrc extends StatelessWidget {
                           Padding(
                             padding: const EdgeInsets.fromLTRB(10, 2, 10, 10),
                             child: TextField(
-                              onChanged: (newVal) {
-                                currentNegotiation.resistance = newVal as int?;
-                              },
                               controller: TextEditingController(),
                               obscureText: false,
                               textAlign: TextAlign.start,
