584146b288b175a010299cb7a90f067960acdba9
Ethan <ethan.tomlinson5912@gmail.com>
2023-01-14 20:47:10 -0500

    This commit is an update on the Determine Issues page. Made an animated list to where you can add and remove various issues. There is some sort of problem with it however where the next bar is having trouble to where it can't be displayed properly so I temporarily removed it. This might be fixed by wrapping the animated list with a container or something idk. Also the names of the issues at this current point cannot be edited by the user, and we also might want to change the add issues button.

--- lib/DetermineIssues.dart
+++ lib/DetermineIssues.dart
@@ -1,194 +1,89 @@
 ///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:negotiation_tracker/StartNewNegotiation.dart';
 import 'package:negotiation_tracker/WeightIssues.dart';
 
 import 'main.dart';
 
-class DetermineIssues extends StatelessWidget {
-  const DetermineIssues({super.key});
+class DetermineIssues extends StatefulWidget {
+  const DetermineIssues({Key? key}) : super(key: key);
+
+  @override
+  _DetermineIssuesState createState() => _DetermineIssuesState();
+}
+
+class _DetermineIssuesState extends State<DetermineIssues> {
+
+  final _items = [];
+
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+
+  void _addIssues() {
+    print('Add issues button clicked');
+    _items.insert(0, "Issue ${_items.length + 1}");
+    _key.currentState!.insertItem(0, duration: const Duration(milliseconds: 300));
+  }
+
+  void _removeIssue(int index) {
+    _key.currentState!.removeItem(index, (_, animation) {
+      return SizeTransition(
+        sizeFactor: animation,
+        child: const Card(
+          margin: EdgeInsets.all(10),
+          elevation: 10,
+          color: Color(0xFFDB7877),
+          child: ListTile(
+            contentPadding: EdgeInsets.all(15),
+          ),
+        ),
+      );
+    }, duration : const Duration(milliseconds: 300));
+    _items.removeAt(index);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffffffff),
       appBar: const PrepareBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Container(
-            margin: const EdgeInsets.all(0),
-            padding: const EdgeInsets.all(0),
-            width: MediaQuery.of(context).size.width,
-            height: 80,
-            decoration: BoxDecoration(
-              color: const Color(0x1f000000),
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.zero,
-              border: Border.all(color: const Color(0xa5000000), width: 1),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: Text(
-                    "Step 1/3",
-                    textAlign: TextAlign.start,
-                    overflow: TextOverflow.clip,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontStyle: FontStyle.normal,
-                      fontSize: 28,
-                      color: Color(0xff000000),
-                    ),
+      //bottomNavigationBar: NextBar(const WeightIssues()),
+
+      body: AnimatedList(
+        key: _key,
+        initialItemCount: 0,
+        padding: const EdgeInsets.all(10),
+        itemBuilder: (_, index, animation) {
+          return SizeTransition(
+            key: UniqueKey(),
+            sizeFactor: animation,
+            child: Card(
+              margin: const EdgeInsets.all(10),
+              elevation: 10,
+              color: const Color(0xff4d4d4d),
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(15),
+                title: Text(_items[index],
+                  style:
+                    const TextStyle(fontSize: 24, color: Colors.white)),
+                trailing: IconButton(
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Color(0xFFDB7877),
                   ),
+                  onPressed: () => _removeIssue(index),
                 ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
-                  child: Text(
-                    "Determine the Issues",
-                    textAlign: TextAlign.start,
-                    overflow: TextOverflow.clip,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontStyle: FontStyle.normal,
-                      fontSize: 18,
-                      color: Color(0xff000000),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(10, 0, 15, 0),
-            child: Text(
-              "An issue is something the negotiators will try to reach agreement on. Consider all the issues relevant to their negotiation. Be sure to include any issues that could make the deal better for you and/or your counterpart.",
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.clip,
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-                fontSize: 18,
-                color: Color(0xff000000),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "1st Issue",
-                        textAlign: TextAlign.start,
-                        overflow: TextOverflow.clip,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                    child: TextField(
-                      controller: TextEditingController(),
-                      obscureText: false,
-                      textAlign: TextAlign.start,
-                      maxLines: 1,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 14,
-                        color: Color(0xff000000),
-                      ),
-                      decoration: InputDecoration(
-                        disabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                          borderSide:
-                              const BorderSide(color: Color(0xff000000), width: 1),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                          borderSide:
-                              const BorderSide(color: Color(0xff000000), width: 1),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                          borderSide:
-                              const BorderSide(color: Color(0xff000000), width: 1),
-                        ),
-                        labelStyle: const TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
-                        ),
-                        hintText: "Enter Issue",
-                        hintStyle: const TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
-                        ),
-                        filled: true,
-                        fillColor: const Color(0xfff2f2f3),
-                        isDense: false,
-                        contentPadding:
-                            const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: MaterialButton(
-                      onPressed: () {},
-                      color: const Color(0xffffffff),
-                      elevation: 0,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero,
-                        side: BorderSide(color: Color(0xff000000), width: 1),
-                      ),
-                      padding: const EdgeInsets.all(16),
-                      textColor: const Color(0xff000000),
-                      height: 40,
-                      minWidth: 140,
-                      child: const Text(
-                        "Add Issue",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: NextBar(const WeightIssues()),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+              )
+            )
+          );
+        } // item builder
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addIssues,
+        backgroundColor: const Color(0xff4d4d4d),
+        child: const Icon(Icons.add)
       ),
     );
   }
