e49ef2fcc3ed92affe3ae7dcc76b09a000dab3c9
Lcs-Knsk <84292789+Lcs-Knsk@users.noreply.github.com>
2023-08-13 13:13:41 -0400

    fixed start menu

--- lib/Start.dart
+++ lib/Start.dart
@@ -21,99 +21,101 @@ class Start extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xfff1f1f1),
       appBar: const TitleBar(),
-      body: Align( alignment: Alignment.topCenter,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
+      body: SingleChildScrollView(
+        child: Align( alignment: Alignment.topCenter,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
 
-            (kIsWeb) ? WebDisplay() : MobileDisplay(),
+              (kIsWeb) ? WebDisplay() : MobileDisplay(),
 
-            Container(
-              margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-              padding: const EdgeInsets.only(),
-              width: (MediaQuery.of(context).size.width >= SIZE) ? SIZE*.85: MediaQuery.of(context).size.width * .85,
-              decoration: BoxDecoration(
-                color: const Color(0x1f000000),
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(15.0),
-                border: Border.all(color: const Color(0xff0A0A5B), width: 1),
-              ),
-              child: Align(
-                alignment: Alignment.center,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => StartNewNegotiation())
-                    );
-                  },
-                  color: const Color(0xffffffff),
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-                  textColor: const Color(0xff0A0A5B),
-                  height: 40,
-                  minWidth: MediaQuery.of(context).size.width,
-                  child: const Text(
-                    "Start New Negotiation",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      fontStyle: FontStyle.normal,
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                padding: const EdgeInsets.only(),
+                width: (MediaQuery.of(context).size.width >= SIZE) ? SIZE*.85: MediaQuery.of(context).size.width * .85,
+                decoration: BoxDecoration(
+                  color: const Color(0x1f000000),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(15.0),
+                  border: Border.all(color: const Color(0xff0A0A5B), width: 1),
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => StartNewNegotiation())
+                      );
+                    },
+                    color: const Color(0xffffffff),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                    textColor: const Color(0xff0A0A5B),
+                    height: 40,
+                    minWidth: MediaQuery.of(context).size.width,
+                    child: const Text(
+                      "Start New Negotiation",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        fontStyle: FontStyle.normal,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            Container(
-              margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-              padding: const EdgeInsets.all(0),
-              width: (MediaQuery.of(context).size.width >= SIZE) ? SIZE*.85: MediaQuery.of(context).size.width * .85,
-              decoration: BoxDecoration(
-                color: const Color(0x1f000000),
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(15.0),
-                border: Border.all(color: const Color(0xff0A0A5B), width: 1),
-              ),
-              child: Align(
-                alignment: Alignment.center,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MyNegotiations())
-                    );
-                  },
-                  color: const Color(0xffffffff),
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-                  textColor: const Color(0xff0A0A5B),
-                  height: 40,
-                  minWidth: MediaQuery.of(context).size.width,
-                  child: const Text(
-                    "View Current Negotiations",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      fontStyle: FontStyle.normal,
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                padding: const EdgeInsets.all(0),
+                width: (MediaQuery.of(context).size.width >= SIZE) ? SIZE*.85: MediaQuery.of(context).size.width * .85,
+                decoration: BoxDecoration(
+                  color: const Color(0x1f000000),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(15.0),
+                  border: Border.all(color: const Color(0xff0A0A5B), width: 1),
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyNegotiations())
+                      );
+                    },
+                    color: const Color(0xffffffff),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                    textColor: const Color(0xff0A0A5B),
+                    height: 40,
+                    minWidth: MediaQuery.of(context).size.width,
+                    child: const Text(
+                      "View Current Negotiations",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        fontStyle: FontStyle.normal,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-    ));
+            ],
+          ),
+    ),
+      ));
   }
 }
 
